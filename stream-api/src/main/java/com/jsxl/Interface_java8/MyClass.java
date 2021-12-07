fd22cb8ad63bb70746a681252f207407e93f3122
jsxllht <93075861+jsxllht@users.noreply.github.com>
2021-12-07 22:47:19 +0800

    Delete stream-api directory

--- stream-api/src/main/java/com/jsxl/Interface_java8/MyClass.java
+++
@@ -1,7 +0,0 @@
-package com.jsxl.Interface_java8;
-
-public class MyClass {
-    public String getName(){
-        return "java8 新特性MyClass";
-    }
-}
--- stream-api/src/main/java/com/jsxl/Interface_java8/Myfun.java
+++
@@ -1,7 +0,0 @@
-package com.jsxl.Interface_java8;
-
-public interface Myfun {
-    default String getName(){
-        return "java8 新特性Myfun";
-    }
-}
